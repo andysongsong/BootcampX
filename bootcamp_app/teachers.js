07f97b476fee9bf0bbd6516194267920ebd5804e
andysongsong <sxj930916@gmail.com>
2021-08-19 03:37:36 +0000

    use parameterized queries

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -7,20 +7,24 @@ const pool = new Pool({
   database: "bootcampx",
 });
 
-pool
-  .query(
-    `
-SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
+const queryString = `
+SELECT teachers.name as teacher, cohorts.name as cohort, COUNT(*) AS total_assistances
 FROM teachers
 JOIN assistance_requests ON teacher_id = teachers.id
 JOIN students ON student_id = students.id
 JOIN cohorts ON cohort_id = cohorts.id
-WHERE cohorts.name = '${process.argv[2] || "JUL02"}'
+WHERE cohorts.name LIKE $1
+GROUP BY teacher, cohort
 ORDER BY teacher;
-`
-  )
+`;
+const cohortName = process.argv[2];
+const values = [`%${cohortName}%`];
+
+pool
+  .query(queryString, values)
   .then((res) => {
     res.rows.forEach((row) => {
       console.log(`${row.cohort}: ${row.teacher}`);
     });
-  });
+  })
+  .catch((err) => console.error("query error", err.stack));
